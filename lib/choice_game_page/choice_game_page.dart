dfd7e2bd5d47a049b243ac951d97805b4a6dde1d
Karina Gordia <kgordya24@gmail.com>
2024-06-11 15:24:17 +0200

    choiceGamePage refactor

--- lib/choice_game_page/choice_game_page.dart
+++ lib/choice_game_page/choice_game_page.dart
@@ -1,71 +1,92 @@
 import 'package:achievements/resources/resources.dart';
 import 'package:flutter/material.dart';
 
-enum _OnTapGame { none, theSimsThree, theSimsFour }
+enum _OnTapGame { none, theSimsTwo, theSimsThree, theSimsFour }
 
 class ChoiceGamePage extends StatefulWidget {
-  ChoiceGamePage({super.key});
+  const ChoiceGamePage({super.key});
 
   @override
   State<ChoiceGamePage> createState() => _ChoiceGamePageState();
 }
 
 class _ChoiceGamePageState extends State<ChoiceGamePage> {
-  _OnTapGame _chosenGame = _OnTapGame.none;
+  _OnTapGame _currentGame = _OnTapGame.none;
+
+  Color changeSelectGameColor(_OnTapGame selectedGame) {
+    if (_currentGame == selectedGame) return const Color(0xff4e6685);
+    return Colors.transparent;
+  }
+
+  void _selectGame(_OnTapGame selectedGame) {
+    setState(() {
+      _currentGame = selectedGame;
+      Navigator.of(context).pushReplacementNamed('/main_page');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        constraints: const BoxConstraints.expand(),
-        color: const Color(0xFF748CAB),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _chosenGame = _OnTapGame.theSimsThree;
-                  });
-                },
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: _chosenGame == _OnTapGame.theSimsThree
-                        ? Color(0xff4e6685)
-                        : Colors.transparent,
-                  ),
-                  child: Image(
-                    image: AssetImage(AppImages.theSims3Logo),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _chosenGame = _OnTapGame.theSimsFour;
-                  });
-                },
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: _chosenGame == _OnTapGame.theSimsFour
-                        ? Color(0xff4e6685)
-                        : Colors.transparent,
-                  ),
-                  child: Image(
-                    image: AssetImage(AppImages.sims4LogoPrimaryWhiteRgbTransparent1),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+      backgroundColor: const Color(0xFF748CAB),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF748CAB),
+        centerTitle: true,
+        title: Text('Select the game',style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),),
+      ),
+      body: GridView.count(
+        crossAxisCount: 1,
+        children: [
+          GameCardWidget(
+            color: changeSelectGameColor(_OnTapGame.theSimsTwo),
+            selectedGame: _OnTapGame.theSimsTwo,
+            imagePath: AppImages.theSims2Logo,
+            onTapActions: () => _selectGame(_OnTapGame.theSimsTwo),
+          ),
+          GameCardWidget(
+            color: changeSelectGameColor(_OnTapGame.theSimsThree),
+            selectedGame: _OnTapGame.theSimsThree,
+            imagePath: AppImages.theSims3Logo,
+            onTapActions: () => _selectGame(_OnTapGame.theSimsThree),
+          ),
+          GameCardWidget(
+            color: changeSelectGameColor(_OnTapGame.theSimsFour),
+            selectedGame: _OnTapGame.theSimsFour,
+            imagePath: AppImages.sims4LogoPrimaryWhiteRgbTransparent1,
+            onTapActions: () => _selectGame(_OnTapGame.theSimsFour),
+          ),
+        ],
       ),
     );
   }
 }
 
+class GameCardWidget extends StatelessWidget {
+  const GameCardWidget(
+      {super.key,
+      required this.color,
+      required this.selectedGame,
+      required this.imagePath,
+      this.onTapActions});
+
+  final Color color;
+  final String imagePath;
+  final _OnTapGame selectedGame;
+  final Function()? onTapActions;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTapActions,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          color: color,
+        ),
+        child: Image(
+          image: AssetImage(imagePath),
+        ),
+      ),
+    );
+  }
+}

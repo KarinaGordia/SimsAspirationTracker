9a565e6927d1c0e7427c9c5d9a450ca90c8010d7
Karina Gordia <kgordya24@gmail.com>
2024-06-28 15:29:16 +0200

    trying new method

--- lib/achievements_page/wish_list_builder.dart
+++ lib/achievements_page/wish_list_builder.dart
@@ -1,3 +1,5 @@
+import 'package:achievements/achievements_page/achievements_page_lists.dart';
+import 'package:achievements/resources/resources.dart';
 import 'package:flutter/material.dart';
 
 class WishListBuilder extends StatelessWidget {
@@ -11,7 +13,7 @@ class WishListBuilder extends StatelessWidget {
     return GridView.builder(
       itemCount: wishList.length,
       gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
+        maxCrossAxisExtent: 190,
         childAspectRatio: 0.85,
       ),
       itemBuilder: (BuildContext context, int index) =>
@@ -25,41 +27,64 @@ class WishCardWidget extends StatelessWidget {
 
   final WishModel wish;
 
+  Image _getExpansionPackImage() {
+    var result = AchievementPageLists.expansionPacks
+        .where((pack) => pack.key == wish.expansionPackKey).toList();
+
+    if (result.isNotEmpty) {
+      return result[0].image;
+    }
+
+    return Image.asset(AppImages.build24dpFill0Wght400Grad0Opsz24);
+  }
+
   @override
   Widget build(BuildContext context) {
-    const goldColor = Color(0xFFE1B047);
+    const goldColor = Color(0xFFE1B047); //0xFFD1CBC1
     return Card.outlined(
       color: Color(0xFFD1CBC1),
       clipBehavior: Clip.hardEdge,
       child: InkWell(
         onTap: () {},
-        child: Column(
+        child: Stack(
           children: [
-            Expanded(
-              flex: 3,
-              child: Image(
-                image: AssetImage(wish.imagePath),
-                width: 54,
-                height: 54,
-              ),
+            Positioned(
+              top: 5,
+              left: 5,
+              width: 25,
+              height: 25,
+              child: _getExpansionPackImage(),
             ),
-            Expanded(
-              flex: 2,
-              child: DecoratedBox(
-                decoration: const BoxDecoration(
-                  color: Colors.white24,
+            Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Image(
+                    image: AssetImage(wish.imagePath),
+                    width: 54,
+                    height: 54,
+                  ),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 3),
-                  child: Center(
-                    child: Text(
-                      wish.name,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(fontSize: 13.5),
+                Expanded(
+                  flex: 2,
+                  child: DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: Colors.white24,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 3),
+                      child: Center(
+                        child: Text(
+                          wish.name,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(fontSize: 13.5),
+                          overflow: TextOverflow.fade,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
           ],
         ),

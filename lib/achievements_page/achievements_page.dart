eb0832f5a0431546a8e6929b0691768128aa2b54
Karina Gordia <kgordya24@gmail.com>
2024-07-02 14:06:43 +0200

    add menuAnchor example

--- lib/achievements_page/achievements_page.dart
+++ lib/achievements_page/achievements_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 
 import 'filter_menu/expansion_pack_model.dart';
 
+enum SampleItem { itemOne, itemTwo, itemThree }
+
 class AchievementsPage extends StatefulWidget {
   const AchievementsPage({super.key, required this.gameId});
 
@@ -20,6 +22,8 @@ class _AchievementsPageState extends State<AchievementsPage> {
 
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  SampleItem? selectedMenu;
+
   void _openEndDrawer() {
     _scaffoldKey.currentState!.openEndDrawer();
   }
@@ -27,8 +31,6 @@ class _AchievementsPageState extends State<AchievementsPage> {
   @override
   void initState() {
     super.initState();
-    print('flagWishes = wishes');
-
     _flagWishes.addAll(AchievementPageLists.wishes);
   }
 
@@ -38,6 +40,34 @@ class _AchievementsPageState extends State<AchievementsPage> {
       key: _scaffoldKey,
       appBar: AppBar(
         actions: [
+          MenuAnchor(
+            builder:
+                (BuildContext context, MenuController controller, Widget? child) {
+              return IconButton(
+                onPressed: () {
+                  if (controller.isOpen) {
+                    controller.close();
+                  } else {
+                    controller.open();
+                  }
+                },
+                icon: const Icon(Icons.more_horiz),
+                tooltip: 'Change the game',
+              );
+            },
+            alignmentOffset: const Offset(-24, 0),
+            style: const MenuStyle(
+              //alignment: Alignment.bottomLeft,
+            ),
+            menuChildren: List<MenuItemButton>.generate(
+              3,
+                  (int index) => MenuItemButton(
+                onPressed: () =>
+                    setState(() => selectedMenu = SampleItem.values[index]),
+                child: Text('The Sims ${index + 2}'),
+              ),
+            ),
+          ),
           IconButton(
             onPressed: _openEndDrawer,
             icon: const Icon(
@@ -61,17 +91,14 @@ class _AchievementsPageState extends State<AchievementsPage> {
       ),
       onEndDrawerChanged: (isOpen) {
         _refreshPage(isOpen);
-        print('end drawer callback isOpen=$isOpen');
       },
     );
   }
 
   void _refreshPage(bool isOpen) {
-    if(_toggledPacks.isEmpty && !isOpen){
+    if (_toggledPacks.isEmpty && !isOpen) {
       _flagWishes.clear();
       _flagWishes.addAll(AchievementPageLists.wishes);
-      print('page was refreshed');
-      print('_flagWishes.length ${_flagWishes.length}');
       setState(() {});
     }
   }

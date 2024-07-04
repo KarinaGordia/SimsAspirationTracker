f7cc4f503e300c9b186295a38ee49dcc13af937b
Karina Gordia <kgordya24@gmail.com>
2024-07-04 14:39:14 +0200

    isComplited wish field

--- lib/achievements_page/wish_card_widget.dart
+++ lib/achievements_page/wish_card_widget.dart
@@ -3,31 +3,42 @@ import 'package:achievements/models/models.dart';
 import 'package:achievements/resources/resources.dart';
 import 'package:flutter/material.dart';
 
-class WishCardWidget extends StatelessWidget {
+class WishCardWidget extends StatefulWidget {
   const WishCardWidget({super.key, required this.wish});
 
   final WishModel wish;
 
+  @override
+  State<WishCardWidget> createState() => _WishCardWidgetState();
+}
+
+class _WishCardWidgetState extends State<WishCardWidget> {
   Image _getExpansionPackImage() {
-    if( ExpansionPackList.theSimsThreeExpansionPacks.containsKey(wish.expansionPackKey)) {
-      return ExpansionPackList.theSimsThreeExpansionPacks[wish.expansionPackKey]!.image;
+    if( ExpansionPackList.theSimsThreeExpansionPacks.containsKey(widget.wish.expansionPackKey)) {
+      return ExpansionPackList.theSimsThreeExpansionPacks[widget.wish.expansionPackKey]!.image;
     }
 
     return Image.asset(AppImages.build24dpFill0Wght400Grad0Opsz24);
   }
 
-  Color changeCardColor() {
-    return const Color(0xFFE1B047); //0xFFD1CBC1
+  Color getWishCardColor() {
+    if(!widget.wish.isCompleted) {
+      return const Color(0xFFD1CBC1);
+    }
+
+    return const Color(0xFFE1B047);
   }
 
   @override
   Widget build(BuildContext context) {
     return Card.outlined(
-      color: Color(0xFFD1CBC1),
+      color: getWishCardColor(),
       clipBehavior: Clip.hardEdge,
       child: InkWell(
         onTap: () {
-          wish.isCompleted = !wish.isCompleted;
+          print('${widget.wish.name} is ${widget.wish.isCompleted ? 'not ' : ''}completed');
+          widget.wish.isCompleted = !widget.wish.isCompleted;
+          setState(() {});
         },
         child: Stack(
           children: [
@@ -43,7 +54,7 @@ class WishCardWidget extends StatelessWidget {
                 Expanded(
                   flex: 3,
                   child: Image(
-                    image: AssetImage(wish.imagePath),
+                    image: AssetImage(widget.wish.imagePath),
                     width: 54,
                     height: 54,
                   ),
@@ -58,7 +69,7 @@ class WishCardWidget extends StatelessWidget {
                       padding: const EdgeInsets.symmetric(horizontal: 3),
                       child: Center(
                         child: Text(
-                          wish.name,
+                          widget.wish.name,
                           textAlign: TextAlign.center,
                           style: const TextStyle(fontSize: 13.5),
                           overflow: TextOverflow.fade,
--- lib/models/wish_model.dart
+++ lib/models/wish_model.dart
@@ -2,10 +2,12 @@ class WishModel {
   final String imagePath;
   final String name;
   final String expansionPackKey;
+  bool isCompleted;
 
   WishModel({
     required this.imagePath,
     required this.name,
     required this.expansionPackKey,
+    this.isCompleted = false,
   });
 }

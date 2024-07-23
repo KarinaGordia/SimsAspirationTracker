0ddb129235bd152707852a7ce97cdf0837b1e8b7
Karina Gordia <kgordya24@gmail.com>
2024-07-23 14:12:20 +0200

    rewrite GameList with Game entity

--- lib/app_game_lists/games.dart
+++ lib/app_game_lists/games.dart
@@ -1,34 +1,48 @@
-import 'package:achievements/app_game_lists/app_game_lists.dart';
-import 'package:achievements/models/models.dart';
+import 'package:achievements/domain/entities/game.dart';
 import 'package:achievements/resources/resources.dart';
 
 class GameList {
-  GameList._();
-
-  static final games = <GameModel>[
-    GameModel(
-      id: 2,
+  static final games = <Game>[
+    Game(
       name: 'The Sims 2',
       imageName: AppImages.theSims2Logo,
       iconName: AppIcons.thesims2,
-      wishes: WishList.theSimsTwoWishes,
-      expansionPacks: ExpansionPackList.theSimsTwoExpansionPacks,
     ),
-    GameModel(
-      id: 3,
+    Game(
       name: 'The Sims 3',
       imageName: AppImages.theSims3Logo,
       iconName: AppIcons.theSims3VistaIconByAureltristenD228w8g,
-      wishes: WishList.theSimsThreeWishes,
-      expansionPacks: ExpansionPackList.theSimsThreeExpansionPacks,
     ),
-    GameModel(
-      id: 4,
+    Game(
       name: 'The Sims 4',
       imageName: AppImages.sims4LogoPrimaryWhiteRgbTransparent1,
       iconName: AppIcons.theSims4Icon,
-      wishes: WishList.theSimsFourWishes,
-      expansionPacks: ExpansionPackList.theSimsFourExpansionPacks,
     ),
   ];
+// static final games = <GameModel>[
+//   GameModel(
+//     id: 2,
+//     name: 'The Sims 2',
+//     imageName: AppImages.theSims2Logo,
+//     iconName: AppIcons.thesims2,
+//     wishes: WishList.theSimsTwoWishes,
+//     expansionPacks: ExpansionPackList.theSimsTwoExpansionPacks,
+//   ),
+//   GameModel(
+//     id: 3,
+//     name: 'The Sims 3',
+//     imageName: AppImages.theSims3Logo,
+//     iconName: AppIcons.theSims3VistaIconByAureltristenD228w8g,
+//     wishes: WishList.theSimsThreeWishes,
+//     expansionPacks: ExpansionPackList.theSimsThreeExpansionPacks,
+//   ),
+//   GameModel(
+//     id: 4,
+//     name: 'The Sims 4',
+//     imageName: AppImages.sims4LogoPrimaryWhiteRgbTransparent1,
+//     iconName: AppIcons.theSims4Icon,
+//     wishes: WishList.theSimsFourWishes,
+//     expansionPacks: ExpansionPackList.theSimsFourExpansionPacks,
+//   ),
+// ];
 }

6e3ff38825ca589f4475fde4c6fc5d1557ddc31d
Karina Gordia <kgordya24@gmail.com>
2024-08-01 17:45:51 +0200

    init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:achievements/ui/widgets/app/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
